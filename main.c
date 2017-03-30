094799565787401c37e02de28ba96f43c3445ce8
evcode <evcode@gmail.com>
2017-03-30 10:41:56 +0800

    add format_sockaddr(), not work well

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include <netinet/in.h> // 'addr' struct
+#include <netinet/in.h> // 'sockaddr' struct
 /* const struct sockaddr *指针，指向要绑定给sockfd的协议地址。
 这个地址结构根据地址创建socket时的地址协议族的不同而不同:
 // ipv4:
@@ -33,6 +33,7 @@ struct sockaddr_un {
     char        sun_path[UNIX_PATH_MAX];  // pathname 
 };
 */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -74,9 +75,86 @@ typedef struct trans_struct
 
 typedef void* TransHandler;
 
-/*
+int format_sockaddr(int type, const char* str, struct sockaddr* s)
+{
+	struct sockaddr_in *addr = (struct sockaddr_in *)s;
+	char ip_str[32];
+	int ip_port;
+
+	memset(addr, 0, sizeof(struct sockaddr_in));
+	addr->sin_family = AF_INET; // TODO: only IPv4 now
+	addr->sin_port   = 9000; // TODO: as default
+
+	if (strcmp(str, "ANY") == 0)
+	{
+		addr->sin_addr.s_addr = htonl(INADDR_ANY);
+	}
+	else if (strcmp(str, "localhost") == 0)
+	{
+		addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+	}
+	else
+	{
+	    int nbr = sscanf(str, "%s:%d", ip_str, &ip_port);
+	    if (nbr != 2)
+	    {
+	    	error("Failed to format sockaddr by %s\n", str);
+	    	return -2;
+	    }
+
+		addr->sin_addr.s_addr = inet_addr(ip_str);
+		addr->sin_port        = htons(ip_port);
+	}
+
+	// TODO: crash here inet_ntoa(addr->sin_addr). WHY?????????????????
+	//debug("Format sockaddr=%s:%d\n", inet_ntoa(addr->sin_addr), addr->sin_port);
+	debug("Format sockaddr=%d:%d\n", addr->sin_addr, addr->sin_port);
+
+	return 0;
+}
+
+/* BSD System Calls Manual
+#include <sys/types.h>
+#include <sys/socket.h>
+----------------------------------------------------------------------
 int socket(int domain, int type, int protol);
+           PF_LOCAL        Host-internal protocols, formerly called PF_UNIX,
+           PF_UNIX         Host-internal protocols, deprecated, use PF_LOCAL,
+           PF_INET         Internet version 4 protocols,
+           PF_ROUTE        Internal Routing protocol,
+           PF_KEY          Internal key-management function,
+           PF_INET6        Internet version 6 protocols,
+           PF_SYSTEM       System domain,
+           PF_NDRV         Raw access to network device
+
+           SOCK_STREAM
+           SOCK_DGRAM
+           SOCK_RAW
+RETURN VALUES
+     A -1 is returned if an error occurs, otherwise the return value is a descriptor referencing the
+     socket.
+----------------------------------------------------------------------
+int bind(int sockfd, const struct sockaddr *addr,socklen_t addrlen);
+RETURN VALUES
+     Upon successful completion, a value of 0 is returned.  Otherwise, a value of -1 is returned and the
+     global integer variable errno is set to indicate the error.
+----------------------------------------------------------------------
+int listen(int sockfd, int backlog);
+RETURN VALUES
+     The listen() function returns the value 0 if successful; otherwise the value -1 is returned and the
+     global variable errno is set to indicate the error.
+----------------------------------------------------------------------
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+RETURN VALUES
+     The call returns -1 on error and the global variable errno is set to indicate the error.  If it suc-
+     ceeds, it returns a non-negative integer that is a descriptor for the accepted socket.
+----------------------------------------------------------------------
+int connect(int sockfd, const struct sockaddr *addr,socklen_t addrlen);
+RETURN VALUES
+     Upon successful completion, a value of 0 is returned.  Otherwise, a value of -1 is returned and the
+     global integer variable errno is set to indicate the error.
 */
+
 int opensock(TRANS_TYPE type, Transaction* trans)
 {
 	int domain, socktype, proto;
@@ -92,7 +170,7 @@ int opensock(TRANS_TYPE type, Transaction* trans)
 		error("Failed to create socket, err=%d\n", fd);
 		return -2;
 	}
-	debug("--> Open the trans <%d>\n", fd);
+	debug("Open the trans <%d>\n", fd);
 
 	memset(trans, 0, sizeof(Transaction));
 	trans->trans_domain=DOMAIN_IPv4;
@@ -103,14 +181,11 @@ int opensock(TRANS_TYPE type, Transaction* trans)
 	return 0;
 }
 
-/*
-int bind(int sockfd, const struct sockaddr *addr,socklen_t *addrlen);
-*/
-int bindsock(Transaction* trans, const char* addr)
+int bindsock(Transaction* trans, const char* str)
 {
 	struct sockaddr_in sockaddr;
 
-	if ((trans == NULL) || (addr == NULL) || (strlen(addr) == 0))
+	if ((trans == NULL) || (str == NULL) || (strlen(str) == 0))
 	{
 		error("Bad parameters\n");
 		return -1;
@@ -122,47 +197,23 @@ int bindsock(Transaction* trans, const char* addr)
 		return -1;
 	}
 
-    unsigned char ipaddr[4] = {127, 0, 0, 1};
-	unsigned int ipport = 9000;
-
-	if (strcmp(addr, "local") == 0)
+	int err = format_sockaddr(0, str, &sockaddr);
+	if (err < 0)
 	{
-		// as 127.0.0.1
-	}
-	else
-	{
-	    int nbr = sscanf(addr, "%d.%d.%d.%d:%d", 
-	    	&ipaddr[0], &ipaddr[1], &ipaddr[2], &ipaddr[3], &ipport);
-	    if (nbr != 5)
-	    {
-	    	error("Failed to scan the input IP string\n");
-	    	return -2;
-	    }
-	    debug("Input ip addr=%d.%d.%d.%d:%d\n",
-	    	ipaddr[0], ipaddr[1], ipaddr[2], ipaddr[3], ipport);
+		return -2;
 	}
 
-	memset(&sockaddr, 0, sizeof(struct sockaddr_in));
-	sockaddr.sin_family = AF_INET;
-	debug("test INADDR_ANY=%d, sockaddr len=%d\n", INADDR_ANY, sizeof(struct sockaddr_in));
-	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY); // TODO: change to ipaddr
-	sockaddr.sin_port = htons(ipport);
-	debug("  the sockaddr %d:%d\n", sockaddr.sin_addr.s_addr, sockaddr.sin_port);
-
-	int err = bind(trans->trans_fd, &sockaddr, sizeof(struct sockaddr_in));
+	err = bind(trans->trans_fd, &sockaddr, sizeof(struct sockaddr_in));
 	if (err < 0)
 	{
 		error("Failed to bind, err=%d\n", err);
 		return -2;
 	}
-	debug("--> Succeed to bind\n");
+	debug("Succeed to bind\n");
 
 	return 0;
 }
 
-/*
-int listen(int sockfd, int backlog);
-*/
 int listensock(Transaction* trans)
 {
 	int err = listen(trans->trans_fd, trans->max_conn);
@@ -171,39 +222,34 @@ int listensock(Transaction* trans)
 		error("Failed to listen, err=%d\n", err);
 		return -2;
 	}
-	debug("--> Succeed to listen\n");
+	debug("Succeed to listen\n");
 
 	return 0;
 }
 
-/*
-int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-*/
 int accpetsock(Transaction* trans)
 {
 	// TODO: domain/AP_* comares with 'sockaddr' to judge
 	struct sockaddr_in sockaddr;
+	socklen_t socklen;
 
 	// accept(trans->trans_fd, NULL, NULL); // TODO: what are last 2 params???
-	int err = accept(trans->trans_fd, &sockaddr, sizeof(struct sockaddr_in));
+	int err = accept(trans->trans_fd, &sockaddr, &socklen);
 	if (err < 0)
 	{
 		error("Failed to accept, err=%d\n", err);
 		return -2;
 	}
-	debug("--> Succeed to accept\n");
+	debug("Succeed to accept\n");
 
 	return 0;
 }
 
-/*
-int connect(int sockfd, const struct sockaddr *addr,socklen_t *addrlen);
-*/
-int connectsock(Transaction* trans, const char* addr)
+int connectsock(Transaction* trans, const char* str)
 {
 	struct sockaddr_in sockaddr;
 
-	if ((trans == NULL) || (addr == NULL) || (strlen(addr) == 0))
+	if ((trans == NULL) || (str == NULL) || (strlen(str) == 0))
 	{
 		error("Bad parameters\n");
 		return -1;
@@ -215,40 +261,19 @@ int connectsock(Transaction* trans, const char* addr)
 		return -1;
 	}
 
-    unsigned char ipaddr[4] = {127, 0, 0, 1};
-	unsigned int ipport = 9000;
-
-	if (strcmp(addr, "local") == 0)
+	int err = format_sockaddr(0, str, &sockaddr);
+	if (err < 0)
 	{
-		// as 127.0.0.1
-	}
-	else
-	{
-	    int nbr = sscanf(addr, "%d.%d.%d.%d:%d", 
-	    	&ipaddr[0], &ipaddr[1], &ipaddr[2], &ipaddr[3], &ipport);
-	    if (nbr != 5)
-	    {
-	    	error("Failed to scan the input IP string\n");
-	    	return -2;
-	    }
-	    debug("Input ip addr=%d.%d.%d.%d:%d\n",
-	    	ipaddr[0], ipaddr[1], ipaddr[2], ipaddr[3], ipport);
+		return -2;
 	}
 
-	memset(&sockaddr, 0, sizeof(struct sockaddr_in));
-	sockaddr.sin_family = AF_INET;
-	debug("test INADDR_ANY=%d, sockaddr len=%d\n", INADDR_ANY, sizeof(struct sockaddr_in));
-	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY); // TODO: change to ipaddr
-	sockaddr.sin_port = htons(ipport);
-	debug("  sockaddr %d:%d\n", sockaddr.sin_addr.s_addr, sockaddr.sin_port);
-
-	int err = connect(trans->trans_fd, &sockaddr, sizeof(struct sockaddr_in));
+	err = connect(trans->trans_fd, &sockaddr, sizeof(struct sockaddr_in));
 	if (err < 0)
 	{
 		error("Failed to connect, err=%d\n", err);
 		return -2;
 	}
-	debug("--> Succeed to connect\n");
+	debug("Succeed to connect\n");
 
 	return 0;
 }
@@ -262,11 +287,11 @@ int main (int argc, char* argv[])
 	opensock(TRANS_TCP, &trans);
 	if (argc > 1)
 	{
-		connectsock(&trans, "local"); // 192.168.1.1:33
+		connectsock(&trans, "localhost"); // TODO: not on local 192.168.1.1:33
 	}
 	else // server mode
 	{
-		bindsock(&trans, "local");
+		bindsock(&trans, "192.168.17.162");
 		listensock(&trans);
 		accpetsock(&trans);
 	}
