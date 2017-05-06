c0c4dbffc399d8302d928565516a4e25c4f334f3
evcode <evcode@gmail.com>
2017-05-06 18:18:49 +0800

    epoll can work

--- trans_epoll.c
+++ trans_epoll.c
@@ -121,20 +121,60 @@ RETURN VALUE
        error occurs, epoll_wait() returns -1 and errno is set appropriately.
 */
 
-#define EPOLL_FD_MAXCOUNT 128
-#define EPOLL_MAXEVENT 64
+// blog.csdn.net/xiajun07061225/article/details/9250579
+// TODO: ETrigger
+// TODO: set nonblocking IO
+// TODO: more code review and TEST
+
+#define EPOLL_MAXEVENT 1024
 static int epoll_fd;
+static struct epoll_event epoll_evts[1024];
 
 static TransIoReadyCb io_ready_cb = NULL;
 
 BOOL io_add(int32 fd)
 {
-	return FALSE;
+	struct epoll_event ev;
+
+	ev.events = EPOLLIN;
+	ev.data.fd = fd; // MANDATORY!!
+
+    int err = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+	if (err < 0)
+	{
+		error("Failed to add into epoll, fd=%d\n", fd);
+		say_errno();
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
 BOOL io_remove(int32 fd)
 {
-	return FALSE;
+#if 0
+	/* NOTE: optimise
+		it seems not wise to add/del fd in epoll FREQUENTLY -
+		the epoll fds are maintained using a BR Tree
+		Too frequent modify on BR Tree is not a small cost!!
+	*/
+#else
+	struct epoll_event ev;
+
+	ev.events = EPOLLIN;
+	ev.data.fd = fd;
+
+    // Since Linux 2.6.9, "event" can be specified as NULL when using EPOLL_CTL_DEL
+    int err = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &ev);
+	if (err < 0)
+	{
+		error("Failed to remove from epoll, fd=%d\n", fd);
+		say_errno();
+		return FALSE;
+	}
+#endif
+
+	return TRUE;
 }
 
 void io_addlisten(TransIoReadyCb cb)
@@ -149,7 +189,7 @@ static void _init()
 	if (init)
 		return;
 
-	epoll_fd = epoll_create(EPOLL_FD_MAXCOUNT); // since 2.6.8 the "size" is ignored in Kernel
+	epoll_fd = epoll_create(128); // since 2.6.8 the "size" is ignored in Kernel
 	if (epoll_fd < 0)
 	{
 		error("Failed to request epoll\n");
@@ -164,31 +204,20 @@ void io_scan(int32 fd)
 {
 	_init();
 
-	//
-	struct epoll_event ev;
-	memset(&ev, 0, sizeof(struct epoll_event));
-	ev.events = EPOLLIN;
+	// add the initial fd
+	io_add(fd);
 
-    int err = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
-	if (err < 0)
-	{
-		error("Failed to add into epoll, fd=%d\n", fd);
-		say_errno();
-		return;
-	}
-
-	//
-	int timeout = -1; // -1: blocking, 0: non-blocking, others in millsec
-	int maxevents = EPOLL_MAXEVENT; // TODO: more tricks here???
+	// do epolling
+	int timeout = 5*1000; // -1: blocking, 0: non-blocking, others in millsec
+	int maxevents = sizeof(epoll_evts)/ sizeof(epoll_evts[0]); // TODO: more tricks here???
 
 	BOOL is = TRUE;
 	while(is)
 	{
-		memset(&ev, 0, sizeof(struct epoll_event));
-		int cnt = epoll_wait(epoll_fd, &ev, maxevents, timeout);
+		int cnt = epoll_wait(epoll_fd, epoll_evts, maxevents, timeout);
 		if (cnt < 0)
 		{
-			error("Failed to epolling\n");
+			error("Failure of epolling\n");
 			say_errno();
 			return;
 		}
@@ -200,14 +229,20 @@ void io_scan(int32 fd)
 		}
 		else // "cnt" means the count of read fds
 		{
+			//debug("+ epoll wait and retrieved %d events\n", cnt);
+			if (maxevents == cnt)
+			{
+				debug("+ Warning: epoll reached the max event count");
+			}
+
 			int i;
 			for (i = 0; i < cnt; i ++)
 			{
 				if (io_ready_cb)
-					io_ready_cb(ev.data.fd, TRANS_IO_READABLE);
+					io_ready_cb(epoll_evts[i].data.fd, TRANS_IO_READABLE);
 			}
 		}
 	}
 
-	return FALSE;
+	return TRUE;
 }
